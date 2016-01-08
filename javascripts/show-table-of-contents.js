ddb0c3576e08038c4dee1a6d588e062d64514501
oy65 <oy65@users.noreply.github.com>
2016-01-07 21:12:59 -0800

    Update show-table-of-contents.js

--- javascripts/show-table-of-contents.js
+++ javascripts/show-table-of-contents.js
@@ -1,4 +1,7 @@
 window.onload = function() {
   var targetElement = document.getElementById("table-of-contents");
-  targetElement.innerHTML = "Hello World";
+  //targetElement.innerHTML = "Hello World";
+  
+  var sectionElement = document.getElementsByTagName("section")[0];
+  targetElement.innerHTML = sectionElement.childElementCount;
 }
